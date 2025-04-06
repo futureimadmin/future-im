b1f6da5e9280e6b26a8a5864c907ecd69d47812c
futureimadmin <anantha.kommaraju@futureim.in>
2025-04-06 20:30:56 +0530

    Pointed to Gateway URL

--- future-commerce-ui/src/components/apis/api.js
+++ future-commerce-ui/src/components/apis/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8084/vcom/api/v1';
+const API_BASE_URL = 'http://localhost:8090/vcom/api/v1';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
